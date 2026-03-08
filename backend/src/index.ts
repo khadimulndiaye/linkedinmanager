ae725c89ebc9045757d7407b89cb19a6653948e4
khadimulndiaye <khadimul.ndiaye@gmail.com>
2026-03-08 07:59:47 +0000

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: config.frontendUrl || '*',
+  origin: config.FRONTEND_URL || '*',
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
